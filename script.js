206536a276618f181695abd156dbdb5db7456e8e
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-30 12:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let button=document.getElementsByTagName("button")[0];
-button.addEventListener("click" function
+button.addEventListener("click" function(event){
 	event.preventDefault();
   var selectElement = document.getElementById("colorSelect");
   var selectedOption = selectElement.options[selectElement.selectedIndex];
