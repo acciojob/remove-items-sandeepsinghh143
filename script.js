244e3519058bfc06880857f972dfdb65c7159f82
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-30 12:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button=document.getElementsByTagName("button")[0];
+button.addEventListener("click" function(){
+  var selectElement = document.getElementById("colorSelect");
+  var selectedOption = selectElement.options[selectElement.selectedIndex];
+  if (selectedOption) {
+    selectElement.removeChild(selectedOption);
+  }
+});
